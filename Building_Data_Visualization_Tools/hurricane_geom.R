b7b56c2fa363d3047362d11b7793af9fb1ae882f
Marshall McQuillen <marshallm94@gmail.com>
2020-02-22 12:05:12 -0600

    Add <package>::<function> format

--- Building_Data_Visualization_Tools/hurricane_geom.R
+++ Building_Data_Visualization_Tools/hurricane_geom.R
@@ -1,9 +1,62 @@
-library(ggplot2)
+library(ggpubr)
+library(tidyverse)
+library(lubridate)
 library(geosphere)
-library(gridExtra)
+library(ggmap)
+library(grid)
 
-# testing area
-source('load_data.R')
+# register API key - only needs to be run once
+# con <- file("~/.bash_profile")
+# results <- readLines(con)
+# close(con)
+# 
+# api_key <- str_split(results[grepl("GOOGLE_MAPS_KEY", results)], "=")[[1]][2]
+# register_google(key = api_key, write = TRUE)
+
+load_data <- function() {
+
+  ext_tracks_widths <- c(7, 10, 2, 2, 3, 5, 5, 6, 4, 5, 4, 4, 5, 3, 4, 3, 3, 3,
+			 4, 3, 3, 3, 4, 3, 3, 3, 2, 6, 1)
+
+  ext_tracks_colnames <- c("storm_id", "storm_name", "month", "day",
+			    "hour", "year", "latitude", "longitude",
+			    "max_wind", "min_pressure", "rad_max_wind",
+			    "eye_diameter", "pressure_1", "pressure_2",
+			    paste("radius_34",
+				  c("ne", "se", "sw", "nw"),
+				  sep = "_"),
+			    paste("radius_50",
+				  c("ne", "se", "sw", "nw"),
+				  sep = "_"),
+			    paste("radius_64",
+				  c("ne", "se", "sw", "nw"),
+				  sep = "_"),
+			    "storm_type", "distance_to_land", "final")
+
+  ext_tracks <- readr::read_fwf("data/hurricane_data.txt",
+				readr::fwf_widths(ext_tracks_widths, ext_tracks_colnames),
+				na = "-99")
+
+  # tidy data
+  df <- ext_tracks %>%
+    dplyr::filter(hour == '12') %>%
+    tidyr::pivot_longer(radius_34_ne:radius_64_nw,
+			names_to = 'tmp',
+			values_to = 'value') %>%
+    tidyr::separate('tmp', c('tmp','wind_speed','direction')) %>%
+    tidyr::pivot_wider(names_from = 'direction', values_from = 'value') %>%
+    dplyr::mutate(date = lubridate::ymd_h(paste(year, month, day, hour,
+						sep = '-')),
+		  storm_id = stringr::str_to_title(paste(storm_name, year, sep = '-')),
+		  longitude = -longitude) %>%
+    dplyr::select(-tmp) %>%
+    dplyr::mutate(month = as.numeric(month),
+		  day = as.numeric(day),
+		  hour = as.numeric(hour),
+		  wind_speed = as.factor(wind_speed))
+
+    return(df)
+}
 
 df <- load_data()
 
@@ -11,12 +64,12 @@ ike <- df %>% dplyr::filter(storm_id == 'Ike-2008', day == 13)
 katrina <- df %>% dplyr::filter(storm_id == "Katrina-2005",
 				month == 8,
 				day == 29)
-map <- get_map(ike %>%
-	       select(longitude, latitude) %>%
-	       slice(1),
-	       zoom = 6,
-	       maptype = 'toner-background')
-base_map <- ggmap(map)
+map <- ggmap::get_map(ike %>%
+			dplyr::select(longitude, latitude) %>%
+			dplyr::slice(1),
+		      zoom = 6,
+		      maptype = 'toner-background')
+base_map <- ggmap::ggmap(map)
 
 compute_group_func <- function(data, scales, scale_radii) {
 
@@ -27,18 +80,18 @@ compute_group_func <- function(data, scales, scale_radii) {
 
   # getting radius for each quadrant/direction
   direction <- data %>%
-    select(c(r_ne, r_se, r_sw, r_nw)) %>%
+    dplyr::select(c(r_ne, r_se, r_sw, r_nw)) %>%
     as.list()
 
   frames <- data.frame()
   # creating data for plotting
   for (i in 1:4) {
 
-    new_data <- destPoint(p = center,
-			  b = quadrant[[i]],
-			  d = direction[[i]] * 1852 * scale_radii) %>%
-      as_tibble() %>%
-      rename(x = lon, y = lat)
+    new_data <- geosphere::destPoint(p = center,
+				     b = quadrant[[i]],
+				     d = direction[[i]] * 1852 * scale_radii) %>%
+      tibble::as_tibble() %>%
+      dplyr::rename(x = lon, y = lat)
 
     frames <- rbind(frames, new_data)
 
@@ -46,20 +99,20 @@ compute_group_func <- function(data, scales, scale_radii) {
   frames
 }
 
-StatHurricane <- ggproto("StatHurricane",
-			 Stat,
-			 compute_group = compute_group_func,
-			 required_aes = c('x','y','r_ne','r_se','r_nw','r_sw',
-					  'fill','colour'),
-			 optional_aes = c('scale_radii')
+StatHurricane <- ggplot2::ggproto("StatHurricane",
+				  Stat,
+				  compute_group = compute_group_func,
+				  required_aes = c('x','y','r_ne','r_se',
+						   'r_nw','r_sw','fill','colour'),
+				  optional_aes = c('scale_radii')
 )
 
-GeomHurricane <- ggproto("GeomHurricane",
-			 GeomPolygon,
-			 default_aes = aes(alpha = 0.65, lwd = 0.75),
-			 required_aes = c('x','y','r_ne','r_se','r_nw','r_sw',
-					  'fill','colour'),
-			 optional_aes = c('scale_radii')
+GeomHurricane <- ggplot2::ggproto("GeomHurricane",
+				  GeomPolygon,
+				  default_aes = ggplot2::aes(alpha = 0.65, lwd = 0.75),
+				  required_aes = c('x','y','r_ne','r_se','r_nw',
+						   'r_sw','fill','colour'),
+				  optional_aes = c('scale_radii')
 )
 
 geom_hurricane <- function(mapping = NULL, data = NULL, scale_radii = 1,
@@ -79,34 +132,37 @@ geom_hurricane <- function(mapping = NULL, data = NULL, scale_radii = 1,
 }
 
 plot_radii_1 <- base_map +
-  geom_hurricane(data = ike , aes(x = longitude, y = latitude,
-				     r_ne = ne, r_se = se,
-				     r_nw = nw, r_sw = sw,
-				     fill = wind_speed,
-				     color = wind_speed)) +
-  scale_color_manual(name = "Wind speed (kts)",
-                     values = c("red", "orange", "yellow")) +
-  scale_fill_manual(name = "Wind speed (kts)",
-                    values = c("red", "orange", "yellow")) +
-  ggtitle("Scale Radii = 1")
+  geom_hurricane(data = ike , ggplot2::aes(x = longitude, y = latitude,
+					   r_ne = ne, r_se = se, r_nw = nw, r_sw = sw,
+					   fill = wind_speed,
+					   color = wind_speed)) +
+  ggplot2::scale_color_manual(name = "Wind speed (kts)",
+			      values = c("red", "orange", "yellow")) +
+  ggplot2::scale_fill_manual(name = "Wind speed (kts)",
+			     values = c("red", "orange", "yellow")) +
+  ggplot2::ggtitle("Scale Radii = 1")
 
 plot_radii_0.5 <- base_map +
-  geom_hurricane(data = ike , aes(x = longitude, y = latitude,
-				     r_ne = ne, r_se = se,
-				     r_nw = nw, r_sw = sw,
-				     fill = wind_speed,
-				     color = wind_speed),
+  geom_hurricane(data = ike , ggplot2::aes(x = longitude, y = latitude,
+					   r_ne = ne, r_se = se, r_nw = nw, r_sw = sw,
+					   fill = wind_speed,
+					   color = wind_speed),
 		 scale_radii = 0.5) +
-  scale_color_manual(name = "Wind speed (kts)",
-                     values = c("red", "orange", "yellow")) +
-  scale_fill_manual(name = "Wind speed (kts)",
-                    values = c("red", "orange", "yellow")) +
-  ggtitle("Scale Radii = 0.5")
+  ggplot2::scale_color_manual(name = "Wind speed (kts)",
+			      values = c("red", "orange", "yellow")) +
+  ggplot2::scale_fill_manual(name = "Wind speed (kts)",
+			     values = c("red", "orange", "yellow")) +
+  ggplot2::ggtitle("Scale Radii = 0.5")
 
-both_plots <- grid.arrange(plot_radii_1,
-			   plot_radii_0.5,
-			   ncol = 2,
-			   top = 'Hurricane Ike | September 13, 2008')
+both_plots <- ggpubr::ggarrange(plot_radii_1,
+				plot_radii_0.5,
+				ncol = 2,
+				legend = 'right',
+				common.legend = TRUE)
+
+both_plots <- ggpubr::annotate_figure(both_plots,
+				      top = text_grob('Hurricane Ike | September 13, 2008',
+						      size = 12))
 
 png('Ike_Plot.png', height = 5, width = 8, units = 'in', res = 300)
 plot(both_plots)
